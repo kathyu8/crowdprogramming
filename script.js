1d07dab987a189b2773e4b865f3097cb4402309a
Kathleen Yu <kathyu8@gmail.com>
2016-04-24 03:14:40 -0400

    connected to server

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
-var board_width = 5;
-var board = [];
-var board_length = board_width * 3;
+var board = [[0,0,0,1,0],
+		   	[0,0,0,0,1],
+	 	   	[0,0,0,0,0],
+	 	   	[0,0,0,0,0],
+		   	[1,1,1,0,0],
+		   	[0,0,0,0,0],
+		   	[0,0,0,0,0]];
 
-// Randomized the board, width is adjustable and comets appear at a rate of 20%
-function randomizeBoard() {
-	// Create board
-	for(var i = 0; i < board_length; i++) {
-		row = []
-		for(var j = 0; j < board_width; j++) {
-			row[j] = (Math.random() < 0.18) ? 1 : 0; 
-		}
-		board[i] = row;
-	}
-
-	// Checks to make sure there's no path without comets
-	for(var j = 0; j < board_width; j++) {
-		var hasComet = false;
-		for(var i = 0; i < board_length; i++) {
-			if(board[i][j]) {
-				hasComet = true;
-				break;
-			}
-		}
-		if(!hasComet) { 
-			board[Math.floor(Math.random() * board_length)][j] = 1; 
-		}
-	}
-	console.log(board)
-}
-
-randomizeBoard();
+var board_width = board[0].length;
 
 var cell_width = 100; // pixels
 $("#board").height(board_width * cell_width)
@@ -50,7 +27,7 @@ function drawBoard() {
 // Gives players control of the ship
 var ship_location, ship = -1;
 
-function moveShip(offset) {
+function createShip(offset) {
 	for(var i = 0; i < board_width; i++){
 		if(!board[offset][i]) { 
 			ship_location = i;
@@ -76,70 +53,106 @@ function moveShip(offset) {
 }
 
 // Keeping track the amount the player earned
-var offset, prev_offset, interval_id, startTime, endTime = -1;
+var offset, prev_offset, interval_id, startTime, endTime = 0;
 var money = 0.000;
+
+var is_active = false;
 
 // Animating the board
 function startGame() {
 	startTime = new Date().getTime();
 	prev_offset = Math.floor((startTime/1000.0) % board.length);
+	is_active = true;
 	
-	// Put ship on the board
-	moveShip(prev_offset);
+	// Create ship on the board
+	createShip(prev_offset);
 	
-	interval_id = setInterval(function() {
-		var milliseconds = new Date().getTime();
-		offset = Math.floor((milliseconds/1000.0) % board.length);
+	checkCollide(); 
 
-		for(var i = 0; i < board_width; i++) {
-			var row = (i - offset) % board.length;
-			if(row < 0) row += board.length;
+	// start animating the board
+	var data = {"localship" : ship_location, "active" : is_active, "mediator" : "average"};
+	var key = "bulbasaur";
+	updateBoard(key, data);
+}
+
+// Updates the board with every server call
+function updateBoard(key, data) {
+	
+	$.ajax({
+		url: "http://codingthecrowd.com/counter.php", 
+		dataType: "jsonp",
+		data: {
+			key: key,
+			data: JSON.stringify(data)
+		},
+  		success: function(response) {
+  			
+  			var results = response["results"];
 			
-			for(var j = 0; j < board_width; j++) {
-				var cell = $("#cell_" + i + "_" + j);
-				cell.empty();
+			if(offset != Math.floor(response["time"] % board.length)) {
+				offset = Math.floor(response["time"] % board.length);
+				
+				for(var i = 0; i < board_width; i++) {
+					var row = (i - offset) % board.length;
+					if(row < 0) row += board.length;
+					for(var j = 0; j < board_width; j++) {
+						var cell = $("#cell_" + i + "_" + j);
+						cell.empty();
 
-				// Recolors the board
-				if(board[row][j]) {
-					cell.prepend('<img src="comet.png" height="' + cell_width + '" width="' + cell_width + '"/>');
-					// Give the player some time to look at the board
-					if(milliseconds - startTime < 2000) continue;
-					// Checks for collisions
-					if(i == board_width - 1 && j == ship_location) {
-						endGame(money);
+						// Recolors the board
+						if(board[row][j]) {
+							cell.prepend('<img src="comet.png" height="' + cell_width + '" width="' + cell_width + '"/>');
+						}
 					}
 				}
+				ship.empty();
+				ship.prepend('<img src="dinosaur.png" height="' + cell_width + '" width="' + cell_width + '"/>');
+			}
+
+			if(is_active) {
+				var data = {"localship" : ship_location, "active" : is_active, "mediator" : "average"};
+				updateBoard(key, data);
+			}
+			else {
+				endGame();
 			}
 		}
-		ship.empty();
-		ship.prepend('<img src="dinosaur.png" height="' + cell_width + '" width="' + cell_width + '"/>');
+	});
+}
 
-		// Only add money if a second has passed
-		if(prev_offset != offset) {
-			money += 0.001;
-			prev_offset = offset;
-			$("#amount").text("$" + money.toFixed(3));
+function checkCollide() {
+	interval_id = setInterval(function(){
+		
+		// End Game if ship collised with dinosaur
+		var bottom_row = (offset + 4) % board.length;
+		
+		if(board[bottom_row][ship_location]) {
+			endGame(money);
 		}
 
 		// End Game if player reaches $2
-		if(money.toFixed(3) == 2.0) endGame(money);
-	}, 50);
+		money += 0.001;
+		$("#amount").text("$" + money.toFixed(3));
+		if(money >= 2.0) endGame(money);
+		
+	} ,1000);
 }
 
-// End the Game
 function endGame(money) {
-	endTime = new Date().getTime();
+	is_active = false;
 	clearInterval(interval_id);
-	alert("Thank you for playing! The amount you earned was $" + money.toFixed(2));
+
+	endTime = new Date().getTime();
 	$("#game").hide();
 	$("#thank_you").show();
 	$("#amount_earned").text("You earned $" + money.toFixed(2));
 
+	alert("Thank you for playing! The amount you earned was $" + money.toFixed(2));
+
 	// Cookie with the current time to watch for cheaters
 	$("#cookie").val(endTime);
 	$("#time").val((endTime - startTime)/1000.0);
-	$("#money").val(money.toFixed(2));
-	$("#mturk_form").submit();
+
 }
 
 // Hides the final thank you screen
